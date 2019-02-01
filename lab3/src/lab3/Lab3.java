9ad9d77b967d5a7bfc827ea8686caaba2e1e094a
JasonDeras <derasjason2@gmail.com>
2019-02-01 14:54:27 -0600

    Opciones del menu

--- lab3/src/lab3/Lab3.java
+++ lab3/src/lab3/Lab3.java
@@ -11,7 +11,16 @@ public class Lab3 {
         Connective_line.add(new Connective_Line());
         int r = 1;
         while (r == 1) {
-        }
+            System.out.println("1. Registar administrador");
+            System.out.println("2. Registrar Freelance");
+            System.out.println("3. Agragar Empresas");
+            System.out.println("Ingrese una opcion: ");
+            int op = l.nextInt();
+            switch (op) {
+                case 2:
+                    break;
+            }
+        }//Fin del while del menu
     }
 
 }
